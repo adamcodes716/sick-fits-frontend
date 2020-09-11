cedc8c2a39db72e40465c1de7bc9fca3221d0386
adamcodes716 <adamcodes716@gmail.com>
2020-09-11 13:59:49 -0400

    mutation to create item on backend

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5d75663c0e14a547370c";
+/******/ 	var hotCurrentHash = "8b0d8b35638c937773b1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
