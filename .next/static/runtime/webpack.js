98fb8b7f5b93870293bdf777c4d07450937bfe54
adamcodes716 <adamcodes716@gmail.com>
2020-09-14 19:26:39 -0400

    pagination

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5b88c7beb940e23f2c3a";
+/******/ 	var hotCurrentHash = "ccec4e3c0e9cc2a2a194";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
